520bf5101b1914697b0e9f8e44dbc2a00c5f8d13
Randy Rivera <randyr7k@gmail.com>
2022-02-15 21:05:30 -0400

    Add src and template.

---
+++ src/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.views import generic
+
+class IndexView(generic.TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, self.get_context_data())
